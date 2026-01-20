65fc47341fae5d931d831afa83d55c2bc20b34ae
taeik <kti1452@naver.com>
2026-01-20 20:02:54 +0900

    feat: MemoryMemberRepository 구현

---
+++ src/main/java/com/example/spring_practice/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package com.example.spring_practice.repository;
+
+import com.example.spring_practice.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(sequence, member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(m -> m.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
