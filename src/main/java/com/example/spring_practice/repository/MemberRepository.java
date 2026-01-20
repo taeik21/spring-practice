e85a299118130c9bcf81cdf66042f0fd803b02cd
taeik <kti1452@naver.com>
2026-01-20 19:15:04 +0900

    feat: MemberRepository 인터페이스 구현

---
+++ src/main/java/com/example/spring_practice/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice.repository;
+
+import com.example.spring_practice.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
