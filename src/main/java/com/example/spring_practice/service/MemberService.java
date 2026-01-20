88fc8790ddb0209a719d16f1f17124094dd58930
taeik <kti1452@naver.com>
2026-01-20 20:48:20 +0900

    feat: MemberService 구현

---
+++ src/main/java/com/example/spring_practice/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.example.spring_practice.service;
+
+import com.example.spring_practice.domain.Member;
+import com.example.spring_practice.repository.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m-> {
+                    throw new IllegalStateException("중복된 회원 이름입니다");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id){
+        return memberRepository.findById(id);
+    }
+}
